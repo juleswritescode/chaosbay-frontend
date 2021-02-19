6451bcb3ed4f09f480457ac5af64ff4b35cf6715
Julian <juliandomke@outlook.de>
2021-02-19 21:40:43 +0100

    react helmet support in SSR

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
             },
         },
         'gatsby-plugin-sharp',
+        'gatsby-plugin-react-helmet',
         'gatsby-transformer-sharp',
         'gatsby-plugin-offline',
     ],
